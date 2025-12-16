1645ea243b56e15fc9e2b28366d186157c1f0647
zozimofreitas3-sys <zozimofreitas3@gmail.com>
2025-12-16 03:58:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/drive-links')
+def drive_links():
+    return render_template('drive-links.html')
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
